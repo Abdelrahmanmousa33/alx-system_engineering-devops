f6b1793ce2bd0788216b128925703950fc748e9d
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-25 08:59:18 -0700

    typo fixed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #A manfiest that kills a process
 exec {'killmenow':
-  command  => 'usr/bin/pkill killmwnow',
+  command  => '/usr/bin/pkill killmwnow',
   provider => 'shell',
   returns  => [0, 1],
 }
