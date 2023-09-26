f832951aeacfac6db156919de346ed4f08f888dd
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-09-26 03:22:31 -0700

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
 #changes ssh confugration file 
-include stdlib
 file { '/etc/ssh/ssh_config':
   ensure => present,
 }
 
 file_line { 'turn off password auth':
+  path  => '/etc/ssh/ssh_config',
   line  => '#   PasswordAuthentication no',
   match => '#   PasswordAuthentication',
 }
 
-file_line { 'etc/ssh/ssh_config':
+file_line { 'change identity file':
+  path  => '/etc/ssh/ssh_config',
   line  => '#   IdentityFile ~/.ssh/school',
   match => '#   IdentityFile',
 }
